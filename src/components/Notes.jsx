0db2c82885982108f5882d77abaf3a7b22535721
Yogesh Sanjay Bhavsar <82694875+YogeshSBhavsar@users.noreply.github.com>
2022-09-03 07:07:14 -0700

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>React BootCamp</h1>
+      <p>
+        All the students who complete the BootCamp with complete attendance and
+        project submission will be rewarded certificates from us.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
